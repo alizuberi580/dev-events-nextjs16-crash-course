924cb602a01df0da8946232db0dedfc97c5e6277
alizuberi580 <muhammadali.zuberi10.com>
2026-02-11 23:52:59 +0500

    Api fetch applied

--- app/page.tsx
+++ app/page.tsx
@@ -3,27 +3,27 @@ import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
 import { EventProps } from "@/database/event.model";
 import { cacheLife } from "next/cache";
-import events, { EventItem } from '@/lib/constants';
+//import events, { EventItem } from '@/lib/constants';
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
     'use cache'
     cacheLife('hours')
 
-    // Fetch from API (currently disabled - using static data)
-    // const response = await fetch(`${BASE_URL}/api/events`);
-    // const { events } = await response.json();
+    //Fetch from API (currently disabled - using static data)
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const { events } = await response.json();
 
     return (
         <section>
-            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss </h1>
+            <h1 className="text-center">The Hub for Every Dev <br /> Event You Cant Miss </h1>
             <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in one place</p>
             <ExploreBtn />
 
             <div className={"mt-20 space-y-7"}>
                 <h3>Featured Events</h3>
                 <ul className={"events list-none"}>
-                    {events && events.length > 0 && events.map((event: EventItem) => ( //make EventItem type EventProp after deploying and pushing again to main
+                    {events && events.length > 0 && events.map((event: EventProps) => ( //make EventItem type EventProp after deploying and pushing again to main
                         <li key={event.title} className={"list-none"}>
                             <EventCard {...event} />
                         </li>)
